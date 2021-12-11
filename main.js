fb4412df8635f03713ef1d28310ae685f556523b
karol-musial <nYmina:karol-musial>
2021-12-11 16:15:12 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow
+
+function createWindow () {
+
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadFile('renderer/main.html')
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
